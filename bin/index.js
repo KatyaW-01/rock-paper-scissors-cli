e2f0aeee50801f554984142e99864d0c53935f88
Katya Weicht <katya.weicht@gmail.com>
2025-05-16 10:21:38 -0600

    Add test for update stats

--- bin/index.js
+++ bin/index.js
@@ -1,4 +1,4 @@
-#!/usrbin/env node
+#!/usr/bin/env node
 import { program } from "commander";
 import { gameState } from "../src/lib/state.js";
 import { showMainMenu } from "../src/lib/gameLogic.js";
